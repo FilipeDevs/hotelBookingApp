a99853d3688859d74aa4a9f73fc410fb60d55803
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-12-15 20:53:10 +0100

    Setup main entities and relationship

--- backend/hotel-api/src/main/java/com/filipeDevs/hotelapi/model/BookedRoom.java
+++ backend/hotel-api/src/main/java/com/filipeDevs/hotelapi/model/BookedRoom.java
@@ -1,5 +1,72 @@
 package com.filipeDevs.hotelapi.model;
 
+import java.time.LocalDate;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_in")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out")
+    private LocalDate checkOutDate;
+
+    private String guestFullName;
+
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numberOfAdults;
+
+    @Column(name = "children")
+    private int numberOfChildren;
+
+    @Column(name = "total_guest")
+    private int totalNumberOfGuests;
+
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuests() {
+        this.totalNumberOfGuests = this.numberOfAdults + this.numberOfChildren;
+    }
+
+    public void setNumberOfAdults(int numberOfAdults) {
+        this.numberOfAdults = numberOfAdults;
+        calculateTotalNumberOfGuests();
+    }
+
+    public void setNumberOfChildren(int numberOfChildren) {
+        this.numberOfChildren = numberOfChildren;
+        calculateTotalNumberOfGuests();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 
 }
--- backend/hotel-api/src/main/java/com/filipeDevs/hotelapi/model/Room.java
+++ backend/hotel-api/src/main/java/com/filipeDevs/hotelapi/model/Room.java
@@ -1,5 +1,59 @@
 package com.filipeDevs.hotelapi.model;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomType;
+
+    private BigDecimal roomPrice;
+
+    private boolean isBooked = false;
+
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    @Lob
+    private Blob photo;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking) {
+        if (bookings == null) {
+            bookings = new ArrayList<>();
+        }
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true; // Room assigned to a booking is considered booked
+        String bookingCode = RandomStringUtils.randomNumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+    }
 
 }

9e60db4bf82395a4e95bca777459d778333e0d95
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-12-16 14:06:05 +0100

    Handle feedback messages for adding a new room

--- client/hotel-client/src/components/room/AddRoom.jsx
+++ client/hotel-client/src/components/room/AddRoom.jsx
@@ -47,6 +47,12 @@ function AddRoom() {
     } catch (error) {
       setErrorMessage(error.message);
     }
+
+    // Clear the feedback messages after 5 seconds
+    setTimeout(() => {
+      setSuccessMessage("");
+      setErrorMessage("");
+    }, 5000);
   };
 
   return (
@@ -55,6 +61,15 @@ function AddRoom() {
         <div className="row justify-content-center">
           <div className="col-md-8 col-lg-6">
             <h2 className="mt-5 mb-2">Add a New Room</h2>
+            {successMessage && (
+              <div className="alert alert-success fade show">
+                {successMessage}
+              </div>
+            )}
+
+            {errorMessage && (
+              <div className="alert alert-danger fade show">{errorMessage}</div>
+            )}
             <form onSubmit={handleSubmit}>
               <div className="mb-3">
                 <label className="form-label" htmlFor="roomType">

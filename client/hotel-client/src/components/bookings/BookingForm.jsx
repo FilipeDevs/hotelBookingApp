148816b3e157857cbf569f93db0fdda244d2db9c
Filipe Pereira <filipe.pereira.dev@gmail.com>
2023-12-18 20:53:04 +0100

    Start set up for booking components

--- client/hotel-client/src/components/bookings/BookingForm.jsx
+++ client/hotel-client/src/components/bookings/BookingForm.jsx
@@ -1,6 +1,106 @@
-import React from "react";
+import React, { Component, useEffect, useState } from "react";
+import { bookRoom, getRoomById } from "../utils/API";
+import { useNavigate, useParams } from "react-router-dom";
+import moment from "moment";
 
 function BookingForm() {
+  const [validated, setValidated] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [roomPrice, setRoomPrice] = useState(0);
+
+  const [booking, setBooking] = useState({
+    guestFullName: "",
+    guestEmail: "",
+    checkInDate: "",
+    checkOutDate: "",
+    numOfAdults: "",
+    numOfChildren: "",
+  });
+
+  const [roomInfo, setRoomInfo] = useState({
+    photo: "",
+    roomType: "",
+    roomPrice: "",
+  });
+
+  const { roomId } = useParams();
+  const navigate = useNavigate();
+
+  const getRoomPriceById = async (roomId) => {
+    try {
+      const response = await getRoomById(roomId);
+      setRoomPrice(response.roomPrice);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setBooking({ ...booking, [name]: value });
+    setErrorMessage("");
+  };
+
+  useEffect(() => {
+    getRoomPriceById(roomId);
+  }, [roomId]);
+
+  const calculateTotalPrice = () => {
+    const checkInDate = moment(booking.checkInDate);
+    const checkOutDate = moment(booking.checkOutDate);
+    const diffInDays = checkOutDate.diff(checkInDate, "days");
+    const paymentPerDay = roomPrice ? roomPrice : 0;
+    return diffInDays * paymentPerDay;
+  };
+
+  // Check if the booking has enough guests and at least one adult
+  const isGuestCountValid = () => {
+    const adultCount = parseInt(booking.numOfAdults);
+    const childrenCount = parseInt(booking.numOfChildren);
+    const totalCount = adultCount + childrenCount;
+    return totalCount >= 1 && adultCount >= 1;
+  };
+
+  const isCheckOutDateValid = () => {
+    if (
+      !moment(booking.checkOutDate).isSameOrAfter(moment(booking.checkInDate))
+    ) {
+      setErrorMessage("Check-out date must be after check-in date");
+      return false;
+    } else {
+      setErrorMessage("");
+      return true;
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (
+      form.checkValidity() === false ||
+      !isGuestCountValid() ||
+      !isCheckOutDateValid()
+    ) {
+      e.stopPropagation();
+    } else {
+      setIsSubmitted(true);
+    }
+    setValidated(true);
+  };
+
+  const handleFormSubmit = async () => {
+    try {
+      const confirmationCode = await bookRoom(roomId, booking);
+      setIsSubmitted(true);
+      navigate("/booking-success", { state: { message: confirmationCode } });
+    } catch (error) {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+      navigate("/booking-success", { state: { error: errorMessage } });
+    }
+  };
+
   return <div>BookingForm</div>;
 }
 
---
+++ client/hotel-client/src/components/bookings/BookingInfo.jsx
@@ -0,0 +1,31 @@
+import moment from "moment";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function BookingInfo({ booking, payment, isFormValid, onConfirm }) {
+  const checkInDate = moment(booking.checkInDate);
+  const checkOutDate = moment(booking.checkOutDate);
+  const numberOfDays = checkOutDate.diff(checkInDate, "days");
+  const [isBookingConfirmed, setIsBookingConfirmed] = useState(false);
+  const [isProcessingPayment, setIsProcessingPayment] = useState(false);
+  const navigate = useNavigate();
+
+  const handleConfirmBooking = () => {
+    setIsProcessingPayment(true);
+    setTimeout(() => {
+      setIsProcessingPayment(false);
+      setIsBookingConfirmed(true);
+      onConfirm();
+    }, 3000);
+  };
+
+  useEffect(() => {
+    if (isBookingConfirmed) {
+      navigate("/booking-success");
+    }
+  }, [isBookingConfirmed, navigate]);
+
+  return <div>BookingInfo</div>;
+}
+
+export default BookingInfo;

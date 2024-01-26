ec373c8eaa29bf86230be6875b683c0b8c39c79a
Filipe Pereira <filipe.pereira.dev@gmail.com>
2024-01-26 09:47:23 +0100

    Change directories + 1st setup for Docker environement

--- README.md
+++ README.md
@@ -4,4 +4,24 @@ Hotel booking web platform built with Spring and React
 
 (Still in developement...)
 
+# Usage
 
+## Spring Boot API
+
+```
+mvn compile
+```
+
+```
+mvn spring-boot:run
+```
+
+## React Client
+
+```
+npm install
+```
+
+```
+npm run dev
+```
--- hotel-api/src/test/java/com/filipeDevs/hotelapi/HotelApiApplicationTests.java
+++ hotel-api/src/test/java/com/filipeDevs/hotelapi/HotelApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.filipeDevs.hotelapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test-h2-db")
 class HotelApiApplicationTests {
 
 	@Test
